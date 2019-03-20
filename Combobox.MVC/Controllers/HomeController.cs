960cb7de1a89aa130c1dd1875031b3f311bb9a7c
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-20 21:56:01 +0300

    getconsole ok

---
+++ Combobox.MVC/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+namespace Combobox.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<JsonResult> GetConsoles()
+        {
+            var product = new List<Console>();
+            HttpClient client = new HttpClient(); 
+            client.BaseAddress = new Uri("http://localhost:33902");
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/Games");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                product = JsonConvert.DeserializeObject<List<Console>>(data);
+
+            }
+            else
+            {
+
+            }
+            return Json(product, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+        public class Console
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public class Game
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal ConsoleId { get; set; }
+        }
+        public class Shop
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal GameId { get; set; }
+        }
+
+
+
+    }
+}
--- ComboboxDoldurma/Controllers/GamesController.cs
+++ ComboboxDoldurma/Controllers/GamesController.cs
@@ -35,12 +35,12 @@ namespace ComboboxDoldurma.Controllers
             new Shop { Id = 4, Name = "Argos", GameId=2 },
             new Shop { Id = 5, Name = "Gamespot", GameId=3 },
             new Shop { Id = 6, Name = "Mall", GameId=3 },
-            new Shop { Id = 6, Name = "Virgin", GameId=4 },
-            new Shop { Id = 6, Name = "GameShop", GameId=5},
-            new Shop { Id = 6, Name = "Virtual Shop", GameId=6 },
-            new Shop { Id = 6, Name = "GameLand", GameId=7 },
-            new Shop { Id = 6, Name = "Sendit", GameId=8 },
-            new Shop { Id = 6, Name = "BuyBuy", GameId=9 }
+            new Shop { Id = 7, Name = "Virgin", GameId=4 },
+            new Shop { Id = 8, Name = "GameShop", GameId=5},
+            new Shop { Id = 9, Name = "Virtual Shop", GameId=6 },
+            new Shop { Id = 10, Name = "GameLand", GameId=7 },
+            new Shop { Id = 11, Name = "Sendit", GameId=8 },
+            new Shop { Id = 12, Name = "BuyBuy", GameId=9 }
        };
         public IEnumerable<Console> Get()
         {
