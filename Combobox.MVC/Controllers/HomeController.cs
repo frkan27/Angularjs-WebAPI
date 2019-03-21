7924dda6e007a0f965ec6a5edc155bcb64dc15b6
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-21 21:09:14 +0300

    ok

--- Combobox.MVC/Controllers/HomeController.cs
+++ Combobox.MVC/Controllers/HomeController.cs
@@ -40,7 +40,51 @@ namespace Combobox.MVC.Controllers
 
         }
 
+        public async Task<JsonResult> GetGames(int ConsoleId)
+        {
+            var games = new List<Game>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress= new Uri("http://localhost:33902");
 
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/Games"+ConsoleId);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                games = JsonConvert.DeserializeObject<List<Game>>(data);
+
+            }
+            else
+            {
+
+            }
+            return Json(games, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public async Task<JsonResult> GetShops(int GameId)
+        {
+            var shops = new List<Shop>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:33902");
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/Games/GetShops/" + GameId);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                shops = JsonConvert.DeserializeObject<List<Shop>>(data);
+
+            }
+            else
+            {
+
+            }
+            return Json(shops, JsonRequestBehavior.AllowGet);
+
+        }
 
 
 
